193a7a66926d38e1fcf62bf1b3aa55b001173c37
Bipul Kumar <bipulkumar@Mac.lan>
2025-12-10 17:25:00 +0530

    Add internal SEO links: tamil story reading keyword linked to homepage across all 209 blog posts

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'TamilKathai';
+export const SITE_DESCRIPTION = 'Welcome to Tamil Kathai - A collection of wonderful Tamil stories.';
+export const SITE_URL = 'https://tamilkathai.in';
+export const SITE_AUTHOR = 'TamilKathai';
+export const SITE_LOCALE = 'ta_IN';
+export const SITE_THEME_COLOR = '#11224E';
